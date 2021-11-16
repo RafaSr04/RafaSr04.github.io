15f535c025e86b11d8ed65edfebd875a021b39e9
RafaSr04 <83562131+RafaSr04@users.noreply.github.com>
2021-11-15 23:52:56 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,7 +2,6 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-
   apiKey: "AIzaSyACWgF-Cqnd2mpnzZKRQN-v-GwVUkhSPi0",
   authDomain: "autentificacion-c441d.firebaseapp.com",
   projectId: "autentificacion-c441d",
@@ -10,4 +9,3 @@ firebase.initializeApp({
   messagingSenderId: "55917251982",
   appId: "1:55917251982:web:4be33a8f8427d26090e8df"
     });
-
